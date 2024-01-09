50a32bc21d7eb1122fc1d57d6362b9c0856fe2ae
ahmetkarlik <ahmetkarlik@vituscommodities.com>
2024-01-09 23:33:15 +0300

    added tests

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from app.routers import greetings
 
-app = FastAPI()
-app.include_router(greetings.router)
+
+def get_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(greetings.router)
+
+    @application.get("/")
+    async def root():
+        return {"message": "Hello Bigger Applications!"}
+
+    return application
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
+app = get_application()
