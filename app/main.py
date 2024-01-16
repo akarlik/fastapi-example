53a718b35432b29cd34bf226e60b8245ff02e736
ahmetkarlik <ahmetkarlik@vituscommodities.com>
2024-01-16 11:44:25 +0300

    added logger

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers import greetings
+from app.utils import logger
 
 
 def get_application() -> FastAPI:
     application = FastAPI()
     application.include_router(greetings.router)
+    application_logger = logger.Logger.make_logger()
+    application.logger = application_logger
 
     @application.get("/")
-    async def root():
+    async def root(request: Request):
+        request.app.logger.info("!!!!!!!!!")
         return {"message": "Hello Bigger Applications!"}
 
     return application
